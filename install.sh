6a6ef4fa98fd6726457e1caed7f21506b57bd575
Epicalert <epicalert@protonmail.com>
2020-06-08 02:25:21 +0800

    added mkdirs to install.sh
    The install script now creates /etc/sws/surveys and /usr/share/sws. Also
    added comments.

--- install.sh
+++ install.sh
@@ -2,8 +2,17 @@
 
 [ ! -n "$1" ] && echo "You must specify an install directory." && exit
 
+#copy cgi scripts to install dir
+#TODO: put surveys.py somewhere else
 sudo cp submit survey surveys.py $1
 
 #TODO: setup script for creating survey tables
 
+#make survey data dir
+sudo mkdir -p /etc/sws/surveys
+
+#make application data dir
+sudo mkdir -p /usr/share/sws
+
+#make survey sql database
 sudo mysql -e "CREATE DATABASE sws;"
